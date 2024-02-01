d1bfc98fb77afbc342651b49e0bd22c03ce36074
Billy <ryotabilly@gmail.com>
2024-01-31 19:35:43 -0800

    added dialog

--- src/components/board/TaskColumn.tsx
+++ src/components/board/TaskColumn.tsx
@@ -1,15 +1,26 @@
 import TaskCard from "./TaskCard";
 import { Task } from ".";
-import { Badge, Box, Typography } from "@mui/material";
+import { Badge, Box, IconButton, Typography } from "@mui/material";
 import { useDroppable } from "@dnd-kit/core";
 import {
   SortableContext,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
+import AddIcon from "@mui/icons-material/Add";
+import TaskUpdateDialog from "./TaskUpdateDialog";
+import { useState } from "react";
 
 function TaskColumn({ tasks, title }: { tasks: Task[]; title: string }) {
   const { setNodeRef } = useDroppable({ id: title });
+  const [addTaskOpen, setAddTaskOpen] = useState<boolean>(false);
   const taskIds = tasks.map((task) => task.id);
+
+  function handleCloseDialog(task: Task | null) {
+    if (task) {
+      //add task here
+    }
+    setAddTaskOpen(false);
+  }
 
   return (
     <Box>
@@ -35,7 +46,12 @@ function TaskColumn({ tasks, title }: { tasks: Task[]; title: string }) {
         >
           <div
             ref={setNodeRef}
-            style={{ minHeight: "600px", minWidth: "340px" }}
+            style={{
+              minHeight: "600px",
+              minWidth: "340px",
+              display: "flex",
+              flexDirection: "column",
+            }}
           >
             {tasks.length > 0 ? (
               tasks.map((task: Task) => <TaskCard key={task.id} task={task} />)
@@ -46,8 +62,18 @@ function TaskColumn({ tasks, title }: { tasks: Task[]; title: string }) {
                 Drop items here
               </div>
             )}
+            <IconButton
+              sx={{
+                maxWidth: 340,
+                borderRadius: 2,
+              }}
+              onClick={() => setAddTaskOpen(true)}
+            >
+              <AddIcon />
+            </IconButton>
           </div>
         </SortableContext>
+        <TaskUpdateDialog open={addTaskOpen} onClose={handleCloseDialog} />
       </Box>
     </Box>
   );
